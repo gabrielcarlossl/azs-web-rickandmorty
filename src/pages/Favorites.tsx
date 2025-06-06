93c775974321d5ed76dddf6479fc7d40574b6e83
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-06-06 13:22:09 -0300

    feat: implement Favorites page to display and manage favorite episodes

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,40 @@
+import { Box, List, ListItem, ListItemText, Paper, Typography, IconButton } from '@mui/material'
+import { useAppSelector } from '../store/configureStore'
+import { useDispatch } from 'react-redux'
+import { removeFavoriteEpisode } from '../store/episodes/actions'
+import StarIcon from '@mui/icons-material/Star'
+import type { Episode } from '../store/episodes/types'
 
 const Favorites = () => {
+  const dispatch = useDispatch()
+  const favorites = useAppSelector(state => state.episode.favorites)
+
   return (
-    <div>Favorites</div>
+    <Box p={4}>
+      <Typography variant="h4" gutterBottom>
+        Episódios Favoritos
+      </Typography>
+      {favorites.length === 0 ? (
+        <Typography>Nenhum episódio favoritado.</Typography>
+      ) : (
+        <List component={Paper} elevation={3}>
+          {favorites.map((ep: Episode) => (
+            <ListItem key={ep.id} divider
+              secondaryAction={
+                <IconButton edge="end" onClick={() => dispatch(removeFavoriteEpisode(ep.id))} aria-label="unfavorite">
+                  <StarIcon color="warning" />
+                </IconButton>
+              }
+            >
+              <ListItemText
+                primary={`${ep.episode} - ${ep.name}`}
+                secondary={`Data de exibição: ${ep.air_date} | ${ep.characters.length} personagens`}
+              />
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Box>
   )
 }
 
