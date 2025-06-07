fc01bc33af4ee7d43ccf6ea221de367d7b81de13
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-06-07 16:00:01 -0300

    feat: add character status and species translation functions

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,33 @@
 // Endpoints
 
 export const RICK_AND_MORTY_API = 'https://rickandmortyapi.com/graphql';
+
+export const CHARACTER_STATUS: {
+  alive: string
+  dead: string
+  unknown: string
+} = {
+  alive: 'Alive',
+  dead: 'Dead',
+  unknown: 'unknown'
+}
+
+export const CHARACTER_SPECIES: {
+  human: string
+  alien: string
+  humanoid: string
+  robot: string
+  unknown: string
+  mythological_creature: string
+  animal: string
+  disease: string
+} = {
+  human: 'Human',
+  alien: 'Alien',
+  humanoid: 'Humanoid',
+  robot: 'Robot',
+  unknown: 'Unknown',
+  mythological_creature: 'Mythological Creature',
+  animal: 'Animal',
+  disease: 'Disease'
+}
